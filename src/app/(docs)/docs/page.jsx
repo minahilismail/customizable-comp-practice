e2686feac645bd6fc63c75d2c88c881aff32c29d
Minahil Ismail <minahilismail44@gmail.com>
2024-10-25 21:50:35 +0500

    (fix):fixed key issue

--- src/app/(docs)/docs/page.jsx
+++ src/app/(docs)/docs/page.jsx
@@ -36,7 +36,7 @@ export default async function Projects() {
           <ul>
             {projects.map(({ title, slug }) => {
               return (
-                <li>
+                <li key={slug}>
                   <Link href={`/docs/${slug}`}>{ title }</Link>
                 </li>
               )
